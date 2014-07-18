b1956127c5f96be549850303f9546d089ce6093f
Sun Kim <sunkim924@gmail.com>
2014-07-18 10:02:23 -0400

    wikistack

--- routes/show.js
+++ routes/show.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models/');
 
+/* route for showing a particular page */
 router.get('/:url_name', function(req, res) {
  models.Page.findOne({url_name: req.params.url_name}, function (err, page) {
    res.render('show', { page: page });
@@ -16,11 +17,18 @@ router.post('/:url_name/delete', function(req, res) {
 });
 
 /* route for editing page */
-router.post('/:url_name/edit', function(req, res) {
-//  //when button clicked,
-//   models.Page.findOneAndUpdate({url_name: req.params.url_name}, function (err, page) {
-//    res.render('add_page', { page: page });
-//  });
-// });
+router.get('/:url_name/edit', function(req, res) {
+  models.Page.findOne({url_name: req.params.url_name}, function (err, page) {
+    res.render('edit_page', { page: page });
+  });
+});
+
+
+/* POST route for submitting edited page */
+router.post('/:url_name/edit/submit', function(req, res) {
+  models.Page.findOneAndUpdate({url_name: req.params.url_name}, {body: req.body.body}, function(err, page) {
+      res.redirect('../');
+  });
+});
 
 module.exports = router;
